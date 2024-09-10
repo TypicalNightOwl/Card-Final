c53e5fc2d59f264dbdc2a5c278027871ac6123fb
Ryan Westhoelter <jwesth1@live.spcollege.edu>
2024-09-09 21:00:10 -0400

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Ryan Westhoelter on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
